f85523c62f2ed4c75d51012c63a6c1ebd1c3784c
Apoorv <tyagi.apoorv133@gmail.com>
2020-08-02 12:20:23 +0530

    initial

--- Wallpaper.py
+++ Wallpaper.py
@@ -9,8 +9,7 @@ def draw_multiple_line_text(image, text, font, text_color, text_start_height):
     lines = textwrap.wrap(text, width=40)
     for line in lines:
         line_width, line_height = font.getsize(line)
-        draw.text(((image_width - line_width) / 2, y_text),
-                  line, font=font, fill=text_color)
+        draw.text(((image_width - line_width) / 2, y_text),line, font=font, fill=text_color)
         y_text += line_height
 
 
